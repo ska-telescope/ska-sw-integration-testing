e3b2c3a7fcf2e3beac152d6e52fd990a9691f1dc
manthan <manthan.ska@gmail.com>
2024-09-16 15:39:51 +0530

    SAH-1593: Update path for feature file

--- Mid/tests/tmc_csp/test_xtp-29387_scan.py
+++ Mid/tests/tmc_csp/test_xtp-29387_scan.py
@@ -12,10 +12,9 @@ from tests.resources.test_harness.subarray_node import SubarrayNodeWrapper
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
-    "../features/tmc_csp/xtp_29387_scan.feature",
+    "../../features/tmc_csp/xtp_29387_scan.feature",
     "TMC executes a Scan command on CSP subarray.",
 )
 def test_scan_command():
