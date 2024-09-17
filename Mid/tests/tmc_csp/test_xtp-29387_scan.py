e3768cb99cb2cc4e617e8e2917b195634170064d
manthan <manthan.ska@gmail.com>
2024-09-17 20:09:15 +0530

    SAH-1593: Update test case

--- Mid/tests/tmc_csp/test_xtp-29387_scan.py
+++ Mid/tests/tmc_csp/test_xtp-29387_scan.py
@@ -11,7 +11,8 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_harness.subarray_node import SubarrayNodeWrapper
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
-@pytest.mark.skip
+
+@pytest.mark.skip(reason="test pipline")
 @pytest.mark.tmc_csp
 @scenario(
     "../../features/tmc_csp/xtp_29387_scan.feature",
