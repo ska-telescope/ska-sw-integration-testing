7aad886b4d38426a3ae5b57a9d27b41ebc6cdd92
Akshay Sonawane <akshayss.ska@gmail.com>
2024-09-20 19:21:44 +0530

    HM-535: update the test cases

--- Low/tests/system_level_tests/test_xtp_xxxxx_telescopestandby.py
+++ Low/tests/system_level_tests/test_xtp_xxxxx_telescopestandby.py
@@ -1,5 +1,5 @@
-"""Test module for TMC StartUp functionality"""
-# import pytest
+"""Test module for TMC-SDP ShutDown functionality"""
+import pytest
 from pytest_bdd import given, scenario, then, when
 from tango import DevState
 
@@ -8,28 +8,27 @@ from Low.tests.resources.test_harness.helpers import (
 )
 
 
-# @pytest.mark.tmc_all
+@pytest.mark.tmc_all
 @scenario(
     "../../features/system_level_tests/xtp_xxxxxx_telescope_startup.feature",
-    "Starting up low telescope",
+    "Standby the low telescope",
 )
-def test_tmc_startup_telescope():
+def test_tmc_csp_standby_telescope():
     """
-    Test case to verify StartUp functionality
+    Test case to verify TMC-CSP Standby functionality
+    Glossary:
+        - "central_node_low": fixture for a TMC CentralNode under test
+        - "simulator_factory": fixture for SimulatorFactory class,
+        which provides simulated subarray and master devices
+        - "event_recorder": fixture for EventRecorder class
     """
 
 
 @given("an low telescope")
 def given_the_sut(central_node_low, subarray_node_low, simulator_factory):
     """
-    Given a TMC
-
-    Args:
-        simulator_factory: fixture for SimulatorFactory class,
-        which provides simulated subarray and master devices
+    Given a TMC and CSP in ON state
     """
-    # central_node_low.csp_master.adminMode = 0
-    # set_admin_mode_values_mccs()
     (_, sdp_master_sim, _) = get_master_device_simulators(simulator_factory)
 
     assert central_node_low.central_node.ping() > 0
@@ -38,15 +37,11 @@ def given_the_sut(central_node_low, subarray_node_low, simulator_factory):
     assert sdp_master_sim.ping() > 0
 
 
-@when("I turn telescope to ON state")
-def move_telescope_to_on(central_node_low):
-    """A method to turn on the telescope."""
-    central_node_low.move_to_on()
-
-
-@then("the SDP, CSP and MCCS goes to ON state")
-def check_devices_is_on(central_node_low, subarray_node_low, event_recorder):
-    """A method to check devices states."""
+@given("an Telescope consisting of SDP, CSP and MCCS that is ON")
+def check_tmc_csp_state_is_on(
+    central_node_low, subarray_node_low, event_recorder
+):
+    """A method to check CentralNode.telescopeState"""
     event_recorder.subscribe_event(central_node_low.csp_master, "State")
     event_recorder.subscribe_event(
         subarray_node_low.subarray_devices["csp_subarray"], "State"
@@ -91,14 +86,17 @@ def check_devices_is_on(central_node_low, subarray_node_low, event_recorder):
     )
 
 
-@then("the telescope goes to state ON")
-def check_telescope_state(central_node_low, event_recorder):
+@when("I invoke standby command on the telescope")
+def move_csp_to_standby(central_node_low):
+    """A method to put CSP to STANDBY"""
+    central_node_low.set_standby()
+
+
+@then("the telescope goes to Standby state")
+def check_telescope_state_standby(central_node_low, event_recorder):
     """A method to check CentralNode.telescopeState"""
-    event_recorder.subscribe_event(
-        central_node_low.central_node, "telescopeState"
-    )
     assert event_recorder.has_change_event_occurred(
         central_node_low.central_node,
         "telescopeState",
-        DevState.ON,
+        DevState.STANDBY,
     )
