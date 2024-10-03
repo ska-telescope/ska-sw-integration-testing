e1cef1aedef7724b7d8e24e64a0e2f1b78b28666
manthan <manthan.ska@gmail.com>
2024-10-03 18:09:42 +0530

    SAH-1599: Update conftest file

--- mid/tests/system_level_tests/conftest.py
+++ mid/tests/system_level_tests/conftest.py
@@ -3,16 +3,83 @@ from typing import Any
 
 import pytest
 from ska_control_model import ObsState
+from ska_integration_test_harness.facades.csp_facade import CSPFacade
+from ska_integration_test_harness.facades.sdp_facade import SDPFacade
 from ska_integration_test_harness.facades.tmc_central_node_facade import (
     TMCCentralNodeFacade,
 )
 from ska_integration_test_harness.facades.tmc_subarray_node_facade import (
     TMCSubarrayNodeFacade,
 )
+from ska_integration_test_harness.inputs.json_input import DictJSONInput
 from ska_integration_test_harness.inputs.test_harness_inputs import (
     TestHarnessInputs,
 )
+from ska_integration_test_harness.structure.telescope_wrapper import (
+    TelescopeWrapper,
+)
+from ska_tango_testing.integration import TangoEventTracer
 from tests.system_level_tests.utils.my_file_json_input import MyFileJSONInput
+
+# ------------------------------------------------------------
+# Test Harness fixtures
+
+DEFAULT_VCC_CONFIG_INPUT = DictJSONInput(
+    {
+        "interface": "https://schema.skao.int/ska-mid-cbf-initsysparam/1.0",
+        "tm_data_sources": [
+            "car://gitlab.com/ska-telescope/ska-telmodel-data?"
+            + "ska-sdp-tmlite-repository-1.0.0#tmdata"
+        ],
+        "tm_data_filepath": (
+            "instrument/ska1_mid_psi/ska-mid-cbf-system-parameters.json",
+        ),
+    }
+)
+
+
+@pytest.fixture
+def default_commands_inputs() -> TestHarnessInputs:
+    """Default JSON inputs for TMC commands."""
+    return TestHarnessInputs(
+        assign_input=MyFileJSONInput("centralnode", "assign_resources_mid"),
+        release_input=MyFileJSONInput("centralnode", "release_resources_mid"),
+        default_vcc_config_input=DEFAULT_VCC_CONFIG_INPUT,
+    )
+
+
+@pytest.fixture
+def central_node_facade(telescope_wrapper: TelescopeWrapper):
+    """Create a facade to TMC central node and all its operations."""
+    central_node_facade = TMCCentralNodeFacade(telescope_wrapper)
+    yield central_node_facade
+
+
+@pytest.fixture
+def subarray_node_facade(telescope_wrapper: TelescopeWrapper):
+    """Create a facade to TMC subarray node and all its operations."""
+    subarray_node = TMCSubarrayNodeFacade(telescope_wrapper)
+    yield subarray_node
+
+
+@pytest.fixture
+def event_tracer() -> TangoEventTracer:
+    """Create an event tracer."""
+    return TangoEventTracer(
+        event_enum_mapping={"obsState": ObsState},
+    )
+
+
+@pytest.fixture
+def csp(telescope_wrapper: TelescopeWrapper):
+    """Create a facade to CSP devices."""
+    return CSPFacade(telescope_wrapper)
+
+
+@pytest.fixture
+def sdp(telescope_wrapper: TelescopeWrapper):
+    """Create a facade to SDP devices."""
+    return SDPFacade(telescope_wrapper)
 
 
 @dataclass
@@ -59,44 +126,44 @@ def context_fixt() -> SubarrayTestContextData:
     return SubarrayTestContextData()
 
 
-def subarray_in_resourcing_state(
-    context_fixt: SubarrayTestContextData,
-    # subarray_id: str,
-    subarray_node_facade: TMCSubarrayNodeFacade,
-    default_commands_inputs: TestHarnessInputs,
-):
-    """Ensure the subarray is in the RESOURCING state."""
-    context_fixt.starting_state = ObsState.RESOURCING
-    context_fixt.expected_next_state = ObsState.IDLE
+# def subarray_in_resourcing_state(
+#     context_fixt: SubarrayTestContextData,
+#     # subarray_id: str,
+#     subarray_node_facade: TMCSubarrayNodeFacade,
+#     default_commands_inputs: TestHarnessInputs,
+# ):
+#     """Ensure the subarray is in the RESOURCING state."""
+#     context_fixt.starting_state = ObsState.RESOURCING
+#     context_fixt.expected_next_state = ObsState.IDLE
 
-    subarray_node_facade.force_change_of_obs_state(
-        ObsState.RESOURCING,
-        default_commands_inputs,
-        wait_termination=True,
-    )
+#     subarray_node_facade.force_change_of_obs_state(
+#         ObsState.RESOURCING,
+#         default_commands_inputs,
+#         wait_termination=True,
+#     )
 
 
-def subarray_in_idle_state(
-    context_fixt: SubarrayTestContextData,
-    # subarray_id: str,
-    subarray_node_facade: TMCSubarrayNodeFacade,
-    central_node_facade: TMCCentralNodeFacade,
-    default_commands_inputs: TestHarnessInputs,
-):
-    """Ensure the subarray is in the IDLE state."""
-    context_fixt.starting_state = ObsState.IDLE
+# def subarray_in_idle_state(
+#     context_fixt: SubarrayTestContextData,
+#     # subarray_id: str,
+#     subarray_node_facade: TMCSubarrayNodeFacade,
+#     central_node_facade: TMCCentralNodeFacade,
+#     default_commands_inputs: TestHarnessInputs,
+# ):
+#     """Ensure the subarray is in the IDLE state."""
+#     context_fixt.starting_state = ObsState.IDLE
 
-    subarray_node_facade.force_change_of_obs_state(
-        ObsState.EMPTY,
-        default_commands_inputs,
-        wait_termination=True,
-    )
+#     subarray_node_facade.force_change_of_obs_state(
+#         ObsState.EMPTY,
+#         default_commands_inputs,
+#         wait_termination=True,
+#     )
 
-    json_input = MyFileJSONInput(
-        "centralnode", "assign_resources_mid"
-    ).with_attribute("subarray_id", 1)
+#     json_input = MyFileJSONInput(
+#         "centralnode", "assign_resources_mid"
+#     ).with_attribute("subarray_id", 1)
 
-    context_fixt.when_action_result = central_node_facade.assign_resources(
-        json_input,
-        wait_termination=True,
-    )
+#     context_fixt.when_action_result = central_node_facade.assign_resources(
+#         json_input,
+#         wait_termination=True,
+#     )
