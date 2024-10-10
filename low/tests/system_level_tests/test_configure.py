73bd6c4598b05db450a673c71c38d391fa5cd501
Muskan Sharma <muskans.ska@gmail.com>
2024-10-10 18:18:18 +0530

    HM-546:Modifed code.

--- low/tests/system_level_tests/test_configure.py
+++ low/tests/system_level_tests/test_configure.py
@@ -1,4 +1,4 @@
-"""Test module for TMC Configure functionality (XTP-xxxxx)"""
+"""Test module for TMC Configure functionality (XTP-66007)"""
 import json
 
 import pytest
@@ -36,6 +36,9 @@ def test_configure_command():
     """
 
 
+# @given("telescope is in ON state") -> conftest
+
+
 @given(parsers.parse("subarray {subarray_id} is in IDLE ObsState"))
 def subarray_in_idle_obsstate(
     central_node_low: CentralNodeWrapperLow,
@@ -45,6 +48,10 @@ def subarray_in_idle_obsstate(
     subarray_id: int,
 ):
     """Checks if SubarrayNode's obsState attribute value is IDLE"""
+    # Subscribe to the obsState change events for each subarray device
+    # (SDP, CSP, MCCS, and TMC). This allows the event tracer to monitor
+    # changes in observation states, enabling timely responses to state
+    # transitions.
     subscribe_to_obsstate_events(event_tracer, subarray_node_low)
     assign_input_json = prepare_json_args_for_centralnode_commands(
         "assign_resources_low", command_input_factory
@@ -56,7 +63,7 @@ def subarray_in_idle_obsstate(
         json.dumps(input_json)
     )
     assert_that(event_tracer).described_as(
-        'FAILED ASSUMPTION IN "WHEN" STEP: '
+        'FAILED ASSUMPTION IN "GIVEN" STEP: '
         "'the subarray is in IDLE obsState'"
         "TMC Central Node device"
         f"({central_node_low.central_node.dev_name()}) "
@@ -67,6 +74,11 @@ def subarray_in_idle_obsstate(
         "longRunningCommandResult",
         (pytest.unique_id[0], COMMAND_COMPLETED),
     )
+    check_subarray_obsstate(
+        subarray_node_low,
+        event_tracer,
+        obs_state=ObsState.IDLE,
+    )
 
 
 @when("I configure it for a scan")
@@ -76,7 +88,7 @@ def invoke_configure(
     command_input_factory,
     event_tracer: TangoEventTracer,
 ):
-    """Invokes End command"""
+    """Invokes Configure command"""
     configure_input_json = prepare_json_args_for_commands(
         "configure_low", command_input_factory
     )
@@ -85,13 +97,13 @@ def invoke_configure(
         configure_input_json
     )
     assert_that(event_tracer).described_as(
-        'FAILED ASSUMPTION IN "WHEN" STEP: '
+        'FAILED ASSUMPTION IN "GIVEN" STEP: '
         "'the subarray is in READY obsState'"
-        "TMC Central Node device"
+        "TMC Subarray Node device"
         f"({subarray_node_low.subarray_node.dev_name()}) "
         "is expected have longRunningCommand as"
         '(unique_id,(ResultCode.OK,"Command Completed"))',
-    ).within_timeout(60).has_change_event_occurred(
+    ).within_timeout(TIMEOUT).has_change_event_occurred(
         subarray_node_low.subarray_node,
         "longRunningCommandResult",
         (pytest.unique_id[0], COMMAND_COMPLETED),
@@ -105,6 +117,9 @@ def subsystem_subarrays_in_configuring(
     subarray_node_low: SubarrayNodeWrapperLow, event_tracer: TangoEventTracer
 ):
     """Check if all subarrays are in CONFIGURING obsState."""
+    # Check if the TMC, CSP, SDP, and MCCS subarrays are in the expected
+    # observation state by verifying the observed state changes for each
+    # subarray device. This function can be used to validate any obsState.
     check_subarray_obsstate(
         subarray_node_low,
         event_tracer,
@@ -117,6 +132,9 @@ def tmc_subarray_ready(
     subarray_node_low: SubarrayNodeWrapperLow, event_tracer: TangoEventTracer
 ):
     """Checks if SubarrayNode's obsState attribute value is READY"""
+    # Check if the TMC, CSP, SDP, and MCCS subarrays are in the expected
+    # observation state by verifying the observed state changes for each
+    # subarray device. This function can be used to validate any obsState.
     check_subarray_obsstate(
         subarray_node_low,
         event_tracer,
--- low/tests/system_level_tests/test_end.py
+++ low/tests/system_level_tests/test_end.py
@@ -7,7 +7,7 @@ from pytest_bdd import given, parsers, scenario, then, when
 from ska_control_model import ObsState
 from ska_tango_testing.integration import TangoEventTracer
 from tests.resources.test_harness.central_node_low import CentralNodeWrapperLow
-from tests.resources.test_harness.helpers import update_eb_pb_ids
+from tests.resources.test_harness.helpers import generate_eb_pb_ids
 from tests.resources.test_harness.subarray_node_low import (
     SubarrayNodeWrapperLow,
 )
@@ -36,6 +36,9 @@ def test_telescope_end():
     """
 
 
+# @given("telescope is in ON state") -> conftest
+
+
 @given(parsers.parse("subarray {subarray_id} is in READY ObsState"))
 def subarray_in_idle_obsstate(
     central_node_low: CentralNodeWrapperLow,
@@ -45,50 +48,64 @@ def subarray_in_idle_obsstate(
     subarray_id: int,
 ):
     """Checks if SubarrayNode's obsState attribute value is EMPTY"""
-    central_node_low.set_subarray_id(subarray_id)
+    # Subscribe to the obsState change events for each subarray device
+    # (SDP, CSP, MCCS, and TMC). This allows the event tracer to monitor
+    # changes in observation states, enabling timely responses to state
+    # transitions.
     subscribe_to_obsstate_events(event_tracer, subarray_node_low)
-    input_json = prepare_json_args_for_centralnode_commands(
+    assign_input_json = prepare_json_args_for_centralnode_commands(
         "assign_resources_low", command_input_factory
     )
-    assign_input_json = update_eb_pb_ids(input_json)
+    input_json = json.loads(assign_input_json)
+    generate_eb_pb_ids(input_json)
     central_node_low.set_serial_number_of_cbf_processor()
-    _, pytest.unique_id = central_node_low.store_resources(assign_input_json)
-    assert subarray_node_low.subarray_node.obsState == ObsState.IDLE
+    _, pytest.unique_id = central_node_low.store_resources(
+        json.dumps(input_json)
+    )
     assert_that(event_tracer).described_as(
-        'FAILED ASSUMPTION IN "WHEN" STEP: '
+        'FAILED ASSUMPTION IN "GIVEN" STEP: '
         "'the subarray is in IDLE obsState'"
         "TMC Central Node device"
         f"({central_node_low.central_node.dev_name()}) "
         "is expected have longRunningCommand as"
         '(unique_id,(ResultCode.OK,"Command Completed"))',
-    ).within_timeout(60).has_change_event_occurred(
+    ).within_timeout(TIMEOUT).has_change_event_occurred(
         central_node_low.central_node,
         "longRunningCommandResult",
         (pytest.unique_id[0], COMMAND_COMPLETED),
     )
+    check_subarray_obsstate(
+        subarray_node_low,
+        event_tracer,
+        obs_state=ObsState.IDLE,
+    )
     configure_input_json = prepare_json_args_for_commands(
         "configure_low", command_input_factory
     )
-    central_node_low.set_serial_number_of_cbf_processor()
     _, pytest.unique_id = subarray_node_low.store_configuration_data(
         configure_input_json
     )
     assert_that(event_tracer).described_as(
-        'FAILED ASSUMPTION IN "WHEN" STEP: '
+        'FAILED ASSUMPTION IN "GIVEN" STEP: '
         "'the subarray is in READY obsState'"
-        "TMC Central Node device"
+        "TMC Subarray Node device"
         f"({subarray_node_low.subarray_node.dev_name()}) "
         "is expected have longRunningCommand as"
         '(unique_id,(ResultCode.OK,"Command Completed"))',
-    ).within_timeout(60).has_change_event_occurred(
+    ).within_timeout(TIMEOUT).has_change_event_occurred(
         subarray_node_low.subarray_node,
         "longRunningCommandResult",
         (pytest.unique_id[0], COMMAND_COMPLETED),
     )
+    check_subarray_obsstate(
+        subarray_node_low,
+        event_tracer,
+        obs_state=ObsState.READY,
+    )
 
 
 @when("I end the configuration")
-def invoke_configure(
+def invoke_end(
     central_node_low: CentralNodeWrapperLow,
     subarray_node_low: SubarrayNodeWrapperLow,
     command_input_factory,
@@ -103,6 +120,9 @@ def subsystem_subarrays_in_configuring(
     subarray_node_low: SubarrayNodeWrapperLow, event_tracer: TangoEventTracer
 ):
     """Check if all subarrays are in IDLE obsState."""
+    # Check if the TMC, CSP, SDP, and MCCS subarrays are in the expected
+    # observation state by verifying the observed state changes for each
+    # subarray device. This function can be used to validate any obsState.
     check_subarray_obsstate(
         subarray_node_low,
         event_tracer,
