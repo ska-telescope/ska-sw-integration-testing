4120cf82fd6232dd62bdf709910f05ab9c9ded6b
manthan <manthan.ska@gmail.com>
2024-10-14 19:33:39 +0530

    SAH-1599: Code cleanup

--- mid/tests/system_level_tests/test_mid_assignresources.py
+++ mid/tests/system_level_tests/test_mid_assignresources.py
@@ -78,9 +78,6 @@ def invoke_assignresources(
     )
 
 
-# test_file.py
-
-
 @then("the TMC, CSP and SDP subarrays transition to RESOURCING obsState")
 def verify_resourcing_state(
     context_fixt: SubarrayTestContextData,
@@ -124,87 +121,3 @@ def verify_idle_state(
         csp.csp_subarray,
         sdp.sdp_subarray,
     )
-
-
-# @then("the TMC, CSP and SDP subarrays transition to RESOURCING obsState")
-# def verify_resourcing_state(
-#     context_fixt: SubarrayTestContextData,
-#     subarray_node_facade: TMCSubarrayNodeFacade,
-#     csp: CSPFacade,
-#     sdp: SDPFacade,
-#     event_tracer: TangoEventTracer,
-# ):
-#     """
-#     Verify the subarray's transition to the RESOURCING state.
-#     """
-#     assert_that(event_tracer).described_as(
-#       f"Both TMC Subarray Node device ({subarray_node_facade.subarray_node})"
-#         f", CSP Subarray device ({csp.csp_subarray}) "
-#         f"and SDP Subarray device ({sdp.sdp_subarray}) "
-#         "ObsState attribute values should move "
-#         f"from {str(context_fixt.starting_state)} to RESOURCING."
-#     ).within_timeout(TIMEOUT).has_change_event_occurred(
-#         subarray_node_facade.subarray_node,
-#         "obsState",
-#         ObsState.RESOURCING,
-#         previous_value=context_fixt.starting_state,
-#     ).has_change_event_occurred(
-#         csp.csp_subarray,
-#         "obsState",
-#         ObsState.RESOURCING,
-#         previous_value=context_fixt.starting_state,
-#     ).has_change_event_occurred(
-#         sdp.sdp_subarray,
-#         "obsState",
-#         ObsState.RESOURCING,
-#         previous_value=context_fixt.starting_state,
-#     )
-
-#     # override the starting state for the next step
-#     context_fixt.starting_state = ObsState.RESOURCING
-
-
-# @then("the CSP, SDP and TMC subarrays must be in IDLE obsState")
-# def verify_idle_state(
-#     context_fixt: SubarrayTestContextData,
-#     subarray_node_facade: TMCSubarrayNodeFacade,
-#     central_node_facade: TMCCentralNodeFacade,
-#     csp: CSPFacade,
-#     sdp: SDPFacade,
-#     event_tracer: TangoEventTracer,
-# ):
-#     """
-#     Verify the subarray's transition to the IDLE state."""
-
-#     assert_that(event_tracer).described_as(
-#       f"Both TMC Subarray Node device ({subarray_node_facade.subarray_node})"
-#         f", CSP Subarray device ({csp.csp_subarray}) "
-#         f"and SDP Subarray device ({sdp.sdp_subarray}) "
-#         "ObsState attribute values should move "
-#         f"from {str(context_fixt.starting_state)} to IDLE."
-#     ).within_timeout(TIMEOUT).has_change_event_occurred(
-#         subarray_node_facade.subarray_node,
-#         "obsState",
-#         ObsState.IDLE,
-#         previous_value=context_fixt.starting_state,
-#     ).has_change_event_occurred(
-#         csp.csp_subarray,
-#         "obsState",
-#         ObsState.IDLE,
-#         previous_value=context_fixt.starting_state,
-#     ).has_change_event_occurred(
-#         sdp.sdp_subarray,
-#         "obsState",
-#         ObsState.IDLE,
-#         previous_value=context_fixt.starting_state,
-#     )
-#     assert_that(event_tracer).described_as(
-#         "TMC Central Node "
-#         f"({central_node_facade.central_node}) "
-#         "is expected to report a"
-#         "longRunningCommand successful completion."
-#     ).within_timeout(TIMEOUT).has_change_event_occurred(
-#         central_node_facade.central_node,
-#         "longRunningCommandResult",
-#         get_expected_long_run_command_result(context_fixt),
-#     )
