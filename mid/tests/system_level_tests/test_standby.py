468876ff19e2580673c8cc4c21ebf8fbd7ac32de
manthan.ska <manthan.ska@gmail.com>
2024-10-15 17:08:37 +0530

    SAH-1594: Test pipeline

--- mid/tests/system_level_tests/test_standby.py
+++ mid/tests/system_level_tests/test_standby.py
@@ -38,8 +38,8 @@ def send_telescope_command(
 
 
 @then(
-    "the Telescope consisting of SDP, CSP devices should \
-        transition to STANDBY state"
+    "the Telescope consisting of SDP and CSP devices "
+    "should transition to STANDBY state"
 )
 def verify_standby_state(
     event_tracer: TangoEventTracer,
