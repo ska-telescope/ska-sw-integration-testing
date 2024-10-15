2cb6d5951401e42eb07096ab4dd1eccc56ce8891
manthan.ska <manthan.ska@gmail.com>
2024-10-15 10:55:53 +0530

    SAH-1594: Revert changes

--- mid/tests/system_level_tests/conftest.py
+++ mid/tests/system_level_tests/conftest.py
@@ -24,9 +24,7 @@ from ska_integration_test_harness.structure.telescope_wrapper import (
 )
 from ska_tango_testing.integration import TangoEventTracer, log_events
 from tango import DevState
-from tests.system_level_tests.utils.json_file_input_handler import (
-    MyFileJSONInput,
-)
+from tests.system_level_tests.utils.my_file_json_input import MyFileJSONInput
 
 # ------------------------------------------------------------
 # Test Harness fixtures
