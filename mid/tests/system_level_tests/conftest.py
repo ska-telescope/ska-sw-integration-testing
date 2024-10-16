c6edab71c74810eac8e25894da1f029246500eb9
manthan.ska <manthan.ska@gmail.com>
2024-10-16 13:30:57 +0530

    SAH-1594: Resolve review comments

--- mid/tests/system_level_tests/conftest.py
+++ mid/tests/system_level_tests/conftest.py
@@ -187,7 +187,7 @@ def check_state_is_off(
     sdp: SDPFacade,
     dishes: DishesFacade,
 ):
-    """Send the ON command to the telescope."""
+    """Send the OFF command to the telescope."""
     central_node_facade.move_to_off(wait_termination=False)
     assert_that(event_tracer).described_as(
         "The telescope SDP,CSP and SDP devices should be in OFF."
@@ -225,49 +225,11 @@ def check_state_is_off(
 
 
 @given("a Telescope consisting of SDP, CSP and DISH that is ON")
-def check_state_is_on(
-    event_tracer: TangoEventTracer,
+def invoke_telescope_on_command(
     central_node_facade: TMCCentralNodeFacade,
-    csp: CSPFacade,
-    sdp: SDPFacade,
-    dishes: DishesFacade,
 ):
     """Send the ON command to the telescope."""
-    event_tracer.clear_events()
-    central_node_facade.move_to_on(wait_termination=False)
-    assert_that(event_tracer).described_as(
-        "The telescope, CSP and SDP devices should be in ON state."
-    ).within_timeout(ASSERTIONS_TIMEOUT).has_change_event_occurred(
-        central_node_facade.central_node,
-        "telescopeState",
-        DevState.ON,
-    ).has_change_event_occurred(
-        csp.csp_master,
-        "State",
-        DevState.ON,
-    ).has_change_event_occurred(
-        csp.csp_subarray,
-        "State",
-        DevState.ON,
-    ).has_change_event_occurred(
-        sdp.sdp_master,
-        "State",
-        DevState.ON,
-    ).has_change_event_occurred(
-        sdp.sdp_subarray,
-        "State",
-        DevState.ON,
-    )
-
-    #  Iterate over dish IDs and verify the transition of each DishMaster
-    for dish_id in ["dish_001", "dish_036", "dish_063", "dish_100"]:
-        assert_that(event_tracer).described_as(
-            f"The DishMaster {dish_id} must transition to STANDBY-FP mode"
-        ).has_change_event_occurred(
-            dishes.dish_master_dict[dish_id],
-            "dishMode",
-            DishMode.STANDBY_FP,
-        )
+    central_node_facade.move_to_on(wait_termination=True)
 
 
 @then("DishMaster must transition to STANDBY-LP mode")
