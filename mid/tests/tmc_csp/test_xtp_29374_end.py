f940ccc28293052324f653d2d93249d08a6e25bd
manthan <manthan.ska@gmail.com>
2024-11-13 16:11:44 +0530

    SAH-1612: test pipline

--- mid/tests/tmc_csp/test_xtp_29374_end.py
+++ mid/tests/tmc_csp/test_xtp_29374_end.py
@@ -17,7 +17,6 @@ from tests.resources.test_harness.utils.common_utils import JsonFactory
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.skip
 @pytest.mark.tmc_csp
 @scenario(
     "../../mid/features/tmc_csp/xtp_29374_end.feature",
