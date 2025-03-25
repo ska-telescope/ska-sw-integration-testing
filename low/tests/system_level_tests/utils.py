dca7dbe354b98bc8c39a4b7f67ef1be2d7ee5304
apurvapatkar <apurva.ska@gmail.com>
2025-03-25 16:46:09 +0530

    SAH-1677: Execute 2 subarray Configure workflow.

--- low/tests/system_level_tests/utils.py
+++ low/tests/system_level_tests/utils.py
@@ -68,13 +68,12 @@ def set_subarray_to_idle(
     # Subscribe to obsState change events
     json = ""
     if subarray_id == "1":
-        # json = "assign_resources_low_real_subarray1_station1"
-        json = "assign_resources_low_real"
+        json = "assign_resources_low_real_subarray1_station1"
+        # json = "assign_resources_low_real"
         LOGGER.info("Subarray Id 1")
     elif subarray_id == "2":
         subarray_node_low = SubarrayNodeWrapperLow("2")
-        # json = "assign_resources_low_real_subarray2_station2"
-        json = "assign_resources_low_real"
+        json = "assign_resources_low_real_subarray2_station2"
         LOGGER.info("Subarray Id 2")
     else:
         LOGGER.info("Invalid Subarray Id")
