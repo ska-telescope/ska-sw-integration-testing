b4feca28e100678a717fe48d70e0165711e3f8e7
manthan <manthan.ska@gmail.com>
2025-04-11 15:42:02 +0530

    SAH-1676: Execute all test cases

--- low/tests/system_level_tests/test_low_abort.py
+++ low/tests/system_level_tests/test_low_abort.py
@@ -25,7 +25,6 @@ from tests.system_level_tests.utils import (
 TIMEOUT = 100
 
 
-@pytest.mark.test1
 @pytest.mark.system_level_tests
 @scenario(
     "system_level_tests/" + "xtp_64112_abort.feature",
